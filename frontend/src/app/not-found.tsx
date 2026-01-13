061bc4168a2883f3324c7362cd92edaf1ad91807
Mirzah-Muzammil <mirzaahmuzammil@gmail.com>
2026-01-13 21:38:00 +0530

    made changes,

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -1,5 +1,9 @@
+import Link from "next/link";
+
 export default function NotFoundPage() {
     return <div className="flex items-center justify-center h-screen">
         <h1 className="text-2xl font-bold text-slate-900">Page not found</h1>
+        
+        <Link href="/login" className="text-violet-600 hover:text-violet-700 font-medium">Back to home</Link>
     </div>
 }
